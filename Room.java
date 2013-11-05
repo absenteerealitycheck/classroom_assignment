4c89db3ba72274c779bd51809f843f832f69de9f
absenteerealitycheck <againessmith@gmail.com>
2013-11-05 01:23:34 -0500

    changed some stuff in isNotAssigned

--- Room.java
+++ Room.java
@@ -174,12 +174,12 @@ public class Room{
 	public boolean isNotAssigned(Tuple<Time, Time> t) {
 		Time start = t.getFirst();
 		Time end = t.getSecond();
-		int sh=start.getEventTime().HOUR_OF_DAY*2;
+		int sh=start.getMilitaryTime()*2;
 		if(start.getMinute()==30)sh++;
-		int eh=end.getEventTime().HOUR_OF_DAY*2;
+		int eh=end.getMilitaryTime()*2;
 		if(end.getMinute()==20)eh++;
 		else if(end.getMinute()==50)eh+=2;
-		int day=t.getFirst().getEventTime().DAY_OF_WEEK-2;
+		int day=t.getFirst().getDay()-2;
 		for(int k=sh;k<eh;k++){
 			if(timeTable[k][day]) return false;
 		}
