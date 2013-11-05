46a8757fb4e26a36dd07f1f98164b3d26301438f
Justyn Pham <jpham14@amherst.edu>
2013-11-05 02:57:38 -0500

    debugging getMilitaryTime() in Time-JP

--- Driver.java
+++ Driver.java
@@ -84,11 +84,7 @@ public class Driver{
 		
 		System.out.println("No missing times?: "+allTimesPresent);
 		
-		/*ArrayList<Tuple<Time,Time>> testTimes = courses.get(0).getPreferredTimes();
-		*rooms.get(0).setTimeTable(testTimes);
-		*rooms.get(0).printTimeTable();
-		*/ //in case you're wondering how the timeTable works in Room
-		
+	
 	}
 	public void linkRoomsToCourses(ArrayList<Course> courses, ArrayList<Room> rooms){
 		/*courses.shuffle
