1a1d121ce1022dc0725685d3178e86e99f84ca6b
Connor Bottum <cbb692@yahoo.com>
2013-12-11 18:18:21 -0500

    added breakup/return code for y2k -cbb

--- Y2K.java
+++ Y2K.java
@@ -3,9 +3,62 @@ public class Y2K {
 	public Y2K(){
 		
 	}
-	public String convert(String name){
-		if(name.equals()||name.equals())
-			return null;
+	public String convert(String nameB){
+		String name=nameB.substring(0, 8);
+		String section=nameB.substring(8);
+		if(name.substring(7).equals("-")){
+			name=name.substring(0, 7);
+			section=nameB.substring(7);
+		}
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
+		if(name.equals())
+			return ""+section;
 		
+		return name+section;
 	}
 }
